d866d1d173abbd8d3d81ba169a453c3e02c24c2c
Ashley Tolen <tolenashley7@gmail.com>
2019-05-15 20:11:37 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,17 @@
 # Add  code here!
+# if prime, return true - if not, false
+def prime?(int)
+  prime = [2, 3, 5, 7]
+  # "Clearly, it is only worthwhile to test candidate factors less than n, and in order from two upwards because an arbitrary n is more likely to be divisible by two than by three, and so on."
+  # If a number is divisible by any of the first prime numbers (2, 3, 5, etc.) then it is not prime.
+  prime.each do |number|
+    if int % number != 0 || int == 2
+      return true
+    # Negative numbers and 1 are not prime
+    elsif int % 2 == 0 || int <= 1
+      return false
+    else
+      return false
+    end
+  end
+end
